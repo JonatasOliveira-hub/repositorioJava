2e634c8829efdedd5acdc0916b380ab6d06f8ff5
Jônatas Oliveira <jonatasoliveira62@yahoo.com.br>
2021-04-25 13:37:44 -0300

    Spring Batch inicial

---
+++ meuprojetobatch/src/test/java/br/com/batch/jonatas/meuprojetobatch/MeuprojetobatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.batch.jonatas.meuprojetobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuprojetobatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
