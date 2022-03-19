4f75b030462536f82f16107be6f525ed174a1d94
Jônatas Oliveira <jonatasoliveira62@yahoo.com.br>
2022-03-19 11:26:13 -0300

    Criação do projeto.

---
+++ especialista-spring/src/main/java/com/especialista/spring/EspecialistaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.especialista.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspecialistaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspecialistaSpringApplication.class, args);
+	}
+
+}
---
+++ especialista-spring/src/test/java/com/especialista/spring/EspecialistaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.especialista.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspecialistaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
