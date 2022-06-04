7a098e6e06434777310867cb9e439fedf035efa3
Jônatas Oliveira <jonatasoliveira62@yahoo.com.br>
2022-06-04 14:11:26 -0300

    Controller gerente.

---
+++ livraria/src/main/java/com/pjs/livraria/livraria/controller/ControllerGerente.java
@@ -0,0 +1,54 @@
+package com.pjs.livraria.livraria.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pjs.livraria.livraria.model.Cliente;
+import com.pjs.livraria.livraria.model.Livro;
+import com.pjs.livraria.livraria.service.ServicoGerente;
+
+@RestController
+@RequestMapping("apiRest/v1/gerente")
+public class ControllerGerente {
+
+	@Autowired
+	private ServicoGerente servicoGerente;
+
+	@PostMapping
+	public ResponseEntity<Cliente> registrarCliente(@RequestBody Cliente cliente) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(servicoGerente.registrarCliente(cliente));
+	}
+
+	@GetMapping
+	public ResponseEntity<Livro> contatarFornecedor(@RequestBody Livro livro) {
+		return ResponseEntity.status(HttpStatus.OK).body(servicoGerente.contatarFornecedor(livro));
+	}
+
+	@GetMapping
+	public ResponseEntity<String> verificarLivro(@RequestBody Livro livro) {
+		return ResponseEntity.status(HttpStatus.OK).body(servicoGerente.verificarLivro(livro));
+	}
+
+	@PutMapping
+	public ResponseEntity<?> realizarTrocaDeLivros() {
+		servicoGerente.realizarTrocaDeLivros();
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+
+	@PostMapping
+	public ResponseEntity<?> encomendarLivros(@RequestBody Livro livro) {
+		servicoGerente.encomendarLivros(livro);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+}
