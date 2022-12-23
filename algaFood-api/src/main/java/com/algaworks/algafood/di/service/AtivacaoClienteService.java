b45cfef6b683b5314233650c96159c8d7be15482
Jônatas Oliveira <jonatasoliveira62@yahoo.com.br>
2022-12-23 18:59:53 -0300

    Event Listeners.

--- algaFood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ algaFood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,6 +1,10 @@
 package com.algaworks.algafood.di.service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
@@ -14,11 +18,32 @@ public class AtivacaoClienteService {
 	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 	@Autowired
 	private Notificador notificador;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	/*
+	 * @PostConstruct //Executado após o construtor, mesmo o padrão, que é vazio;
+	 * public void init() {
+	 * 
+	 * }
+	 * 
+	 * @PreDestroy public void destroy() {
+	 * 
+	 * }
+	 */
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
-			notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		/* publicando envento para o container */
+		publisher.publishEvent(new ClienteAtivadoEvent(cliente));
+		
+		/*
+		 * Ao invés de notificar o cliente, nós vamos notificar o container, dizendo que
+		 * nesse momento, o cliente está ativo. //notificador.notificar(cliente,
+		 * "Seu cadastro no sistema está ativo!");
+		 * 
+		 */
 	}
 
 }
---
+++ algaFood-api/src/main/java/com/algaworks/algafood/listener/NotificacaoService.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood.listener;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.di.notificacao.NivelUrgencia;
+import com.algaworks.algafood.di.notificacao.Notificador;
+import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
+import com.algaworks.algafood.di.service.ClienteAtivadoEvent;
+
+/*Classe que tem interesse em escutar os eventos disparados*/
+@Component
+public class NotificacaoService {
+
+	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
+	@Autowired
+	private Notificador notificador;
+	
+	@EventListener
+	public void clienteAtivadoListener(ClienteAtivadoEvent evento) {
+	
+		notificador.notificar(evento.getCliente(), "Seu cadastro no sistema está ativo.");
+	}
+}
