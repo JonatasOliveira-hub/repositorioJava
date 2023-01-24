9e6df0e5e8972a5f67eb5586a0f31a4cd7c93fc8
Jônatas Oliveira <jonatasoliveira62@yahoo.com.br>
2023-01-24 20:41:07 -0300

    Adionando retorno XML.

--- algaFood-api-modulo3/src/main/java/com/algaworks/algafood/controller/RestauranteController.java
+++ algaFood-api-modulo3/src/main/java/com/algaworks/algafood/controller/RestauranteController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ import com.algaworks.algafood.domain.repository.RestauranteRepository;
 
 
 @RestController
-@RequestMapping("/restaurantes")
+@RequestMapping(value ="/restaurantes" , produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 public class RestauranteController {
 
 	@Autowired
