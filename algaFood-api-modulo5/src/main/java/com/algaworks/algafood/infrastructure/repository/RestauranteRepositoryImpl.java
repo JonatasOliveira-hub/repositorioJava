8471b34e258c8f35ce117fe11183e167bfd058ea
Jônatas Oliveira <jonatasoliveira62@yahoo.com.br>
2023-04-16 19:13:50 -0300

    criteria API.

---
+++ algaFood-api-modulo5/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,41 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.stereotype.Repository;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
+
+@Repository
+public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Override
+	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+		final CriteriaBuilder builder = manager.getCriteriaBuilder();
+
+		final CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+		final Root<Restaurante> root = criteria.from(Restaurante.class);
+		final Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
+		final Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), "%" + taxaFreteInicial + "%");
+		final Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"), "%" + taxaFreteFinal + "%");
+
+		criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
+
+		final TypedQuery<Restaurante> query = manager.createQuery(criteria);
+		return query.getResultList();
+	}
+
+}
