2706a8f5f157a155ac7c9ffe649d6a30579617a8
Jônatas Oliveira <jonatasoliveira62@yahoo.com.br>
2023-06-05 21:03:19 -0300

    Flyway.

--- algaFood-api-modulo5/src/main/java/com/algaworks/algafood/domain/model/Produto.java
+++ algaFood-api-modulo5/src/main/java/com/algaworks/algafood/domain/model/Produto.java
@@ -2,22 +2,40 @@ package com.algaworks.algafood.domain.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-@Entity
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Produto {
 
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
 	private String nome;
-	private String descricao;
-	private BigDecimal preco;
-	private Boolean ativo;
 	
+	@Column(nullable = false)
+	private String descricao;
+	
+	@Column(nullable = false)
+	private BigDecimal preco;
+	
+	@Column(nullable = false)
+	private Boolean ativo;
+
 	@ManyToOne
-	@JoinColumn(name = "restaurante_id")
+	@JoinColumn(nullable = false)
 	private Restaurante restaurante;
 }
